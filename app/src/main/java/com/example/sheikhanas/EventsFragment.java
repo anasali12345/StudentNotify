a7abd645e565cfb7d02cfcb3c3e55d4d9429a55c
Malik Bilal <malikbilal1997@outlook.com>
2020-03-29 00:33:04 +0500

    Update EventsFragment.java

--- app/src/main/java/com/example/sheikhanas/EventsFragment.java
+++ app/src/main/java/com/example/sheikhanas/EventsFragment.java
@@ -1,17 +1,16 @@
 package com.example.sheikhanas;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.sheikhanas.EventModel.EventModel;
 import com.google.firebase.database.DataSnapshot;
@@ -23,27 +22,38 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class EventsFragment extends Fragment {
-     RecyclerView recyclerViewEvent;
-     ArrayList<EventModel> list;
-     EventAdapter eventAdaptor;
-     FirebaseDatabase firebaseDatabase;
-     DatabaseReference databaseReference;
-     LinearLayoutManager linearLayoutManager;
+
+    RecyclerView recyclerViewEvent;
+    ArrayList<EventModel> list;
+    EventAdapter eventAdaptor;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    LinearLayoutManager linearLayoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.fragment_events, container, false);
+        return inflater.inflate(R.layout.fragment_events, container, false);
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         recyclerViewEvent = view.findViewById(R.id.event_recycler_view);
         recyclerViewEvent.setHasFixedSize(true);
+
         linearLayoutManager = new LinearLayoutManager(getContext());
+
         recyclerViewEvent.setLayoutManager(linearLayoutManager);
+
         list = new ArrayList<>();
+
+        eventAdaptor = new EventAdapter(list);
+
+        recyclerViewEvent.setAdapter(eventAdaptor);
+
         RetrieveData();
     }
 
@@ -53,13 +63,14 @@ public class EventsFragment extends Fragment {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String EventImage = snapshot.child("imageUrl").getValue().toString();
-                    String EventDescription = snapshot.child("eventDescription").getValue().toString();
+                    String EventDescription = snapshot.child("eventDiscription").getValue().toString();
                     list.add(new EventModel(EventDescription, EventImage));
                 }
-                eventAdaptor = new EventAdapter(list);
-                recyclerViewEvent.setAdapter(eventAdaptor);
+
+                eventAdaptor.notifyDataSetChanged();
+
             }
 
             @Override
